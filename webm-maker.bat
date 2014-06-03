9887a062ebb64f9dc2abe85d3959d3a48259dcca
Cephei <cepheidev+github@gmail.com>
2014-06-03 02:48:35 +0200

    Added ffmpeg.exe directly to the repository
    Updated script to no longer try and look for an install, instead it now
    uses the bundled version

--- webm-maker.bat
+++ webm-maker.bat
@@ -4,8 +4,6 @@
 setlocal enabledelayedexpansion
 :: Max 4chan file size for webm's, slightly reduced because ffmpeg averages the bitrate and it can become slightly bigger than the max size, even with perfect calculation
 set max_file_size=3000
-:: Custom location to look for ffmpeg, NEEDS TO BE THE FULL PATH
-set customlocation=
 
 :: Check if script was started with a proper parameter ::
 if "%~1" == "" (
@@ -13,33 +11,6 @@ if "%~1" == "" (
 	pause
 	goto :EOF
 )
-
-:: Find ffmpeg folder and its binary file ::
-echo Finding ffmpeg.exe.
-echo Looking up "%~d0\Program Files".
-cd "%~d0\Program Files"
-for /f "delims=" %%f in ('dir /s /b /a-d "ffmpeg.exe"') do (set encoder=%%f)
-if "%encoder%" == "" (
-	echo Trying "%~d0\Program Files (x86)" instead.
-	cd "%~d0\Program Files (x86)"
-	for /f "delims=" %%f in ('dir /s /b /a-d "ffmpeg.exe"') do (set encoder=%%f)
-	if "!encoder!" == "" (
-		if not "%customlocation%" == "" (
-			echo Custom location specified.
-			echo Trying %customlocation% instead.
-			cd /d "%customlocation%"
-			for /f "delims=" %%f in ('dir /s /b /a-d "ffmpeg.exe"') do (set encoder=%%f)
-		)
-	)
-)
-if "%encoder%" == "" (
-	echo Nothing found, make sure ffmpeg is installed and in the proper folders. Default is Program Files and Program Files x86 of the current partition.
-	pause
-	goto :EOF
-)
-:: Ffmpeg was found at this point
-echo ffmpeg.exe was found: "%encoder%"
-echo.
 
 :: Time for some setup
 cd /d "%~dp0"
@@ -63,7 +34,7 @@ set /p length="Enter: " %=%
 if not "%length%" == "" (
 	set lengthset=-t %length%
 ) else (
-	"%encoder%" -i %1 2> webm.tmp
+	ffmpeg.exe -i %1 2> webm.tmp
 	for /f "tokens=1,2,3,4,5,6 delims=:., " %%i in (webm.tmp) do (
 		if "%%i"=="Duration" call :calculatelength %%j %%k %%l %%m
 	)
@@ -76,8 +47,8 @@ set /a bitrate=8*%max_file_size%/%length%
 echo Target bitrate set to %bitrate%
 
 :: Two stage encoding
-"%encoder%" -i "%~1" -c:v libvpx -b:v %bitrate%K -quality best %resolutionset% %startset% %lengthset% -an -threads 0 -f webm -pass 1 -y NUL
-"%encoder%" -i "%~1" -c:v libvpx -b:v %bitrate%K -quality best %resolutionset% %startset% %lengthset% -an -threads 0 -pass 2 -y "%~n1.webm"
+ffmpeg.exe -i "%~1" -c:v libvpx -b:v %bitrate%K -quality best %resolutionset% %startset% %lengthset% -an -threads 0 -f webm -pass 1 -y NUL
+ffmpeg.exe -i "%~1" -c:v libvpx -b:v %bitrate%K -quality best %resolutionset% %startset% %lengthset% -an -threads 0 -pass 2 -y "%~n1.webm"
 del ffmpeg2pass-0.log
 goto :EOF
 
