ecd63f936f6c0fd531d6ff4e11c47e67eea088a4
Cephei <cepheidev+github@gmail.com>
2014-06-03 03:20:54 +0200

    Small fix to auto source video length detect

--- webm-maker.bat
+++ webm-maker.bat
@@ -7,7 +7,8 @@ set max_file_size=3000
 
 :: Check if script was started with a proper parameter ::
 if "%~1" == "" (
-	echo This script needs to be run by dragging and dropping a video file on it. It cannot do anything by itself.
+	echo This script needs to be run by dragging and dropping a video file on it.
+	echo It cannot do anything by itself.
 	pause
 	goto :EOF
 )
@@ -15,31 +16,35 @@ if "%~1" == "" (
 :: Time for some setup
 cd /d "%~dp0"
 :: Ask user how big the webm should be
-echo Webm render resolution. Example: 720 for 720p. Aspect ratio stays the same. Default: Source resolution.
+echo Webm render resolution. Example: 720 for 720p.
+echo Aspect ratio stays the same.
+echo Default: Source resolution.
 set /p resolution="Enter: " %=%
 if not "%resolution%" == "" (
 	set resolutionset=-vf scale=-1:%resolution%
 )
 echo.
 :: Ask user where to start webm rendering in source video
-echo Offset for webm rendering in SECONDS. Default: Start of the source video.
+echo Offset for webm rendering in SECONDS.
+echo Default: Start of the source video.
 set /p start="Enter: " %=%
 if not "%start%" == "" (
 	set startset=-ss %start%
 )
 echo.
 :: Ask user for length of rendering
-echo Webm rendering length in SECONDS. Default: Entire source video.
+echo Webm rendering length in SECONDS.
+echo Default: Entire source video.
 set /p length="Enter: " %=%
 if not "%length%" == "" (
 	set lengthset=-t %length%
 ) else (
-	ffmpeg.exe -i "%1" 2> webm.tmp
+	ffmpeg.exe -i %1 2> webm.tmp
 	for /f "tokens=1,2,3,4,5,6 delims=:., " %%i in (webm.tmp) do (
 		if "%%i"=="Duration" call :calculatelength %%j %%k %%l %%m
 	)
 	del webm.tmp
-	echo Source video length: !length! seconds
+	echo Using source video length: !length! seconds
 )
 echo.
 :: find bitrate that maxes out max filesize on 4chan, defined above
